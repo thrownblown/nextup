7f2dbe4674b02ea483a12ffd0ff1afb94d5b97de
AlexFarioletti <alex@tcbcourier.com>
2014-04-16 15:33:23 -0700

    no \n

--- utils.js
+++ utils.js
@@ -2,25 +2,37 @@ var sanitizer = require('sanitizer');
 
 //http://www.commitstrip.com/en/2014/02/24/coder-on-the-verge-of-extinction/
 
-module.exports.stripHTML = function (html) {
+module.exports.stripHTML = strip = function (html) {
   var clean = sanitizer.sanitize(html, function (str) {
     return str;
   });
-  clean = clean.replace(/<(?:.|\n)*?>/gm, "");
+  clean = clean.replace(/<(?:.|\n)*?>/gm, " ");
   clean = clean.replace(/(?:(?:\r\n|\r|\n|\t)\s*){2,}/ig, " ");
   clean = clean.replace("\\n", " ");
-  clean = clean.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+  clean = clean.replace(/[\.,-\/#!$%\^&\*;:{}=\-_\[\]|`~()]/g," ");
   clean = clean.replace(/\s{2,}/g," ");
-  return clean.trim();
+  clean = clean.replace(/"/g," ");
+
+  return clean;//.trim();
 };
 
-module.exports.replaceAllBackSlash = function (targetStr){
+module.exports.replaceAllBackSlash = noback = function (targetStr){
   var index=targetStr.indexOf('\\');
   while(index >= 0){
-    targetStr=targetStr.replace('\\',"");
+    targetStr=targetStr.replace('\\'," ");
     targetStr=targetStr.replace(/\\/g, " ");
-    targetStr=targetStr.replace("\\n", " ");
     index=targetStr.indexOf('\\');
+  }
+  return targetStr;
+};
+
+module.exports.replaceAllNewLine = noNew = function (targetStr){
+  var index=targetStr.indexOf("\n");
+  while(index >= 0){
+    targetStr=targetStr.replace(/\\n/g, " ");
+    targetStr=targetStr.replace("\\n", " ");
+    targetStr=targetStr.replace("\n", " ");
+    index=targetStr.indexOf("\n");
   }
   return targetStr;
 };
@@ -36,8 +48,20 @@ module.exports.makeJSON = function(str){
   var words = str.split(' ');
   returnObj.wordcount = 0;
   for (var i = 0; i < words.length; i++){
-    if (words[i].length<20&&!parseInt(words[i])){
+
+    if (!commonWords[words[i]] || (words[i].length<20 && !parseInt(words[i]))){
       words[i] = words[i].toLowerCase();
+      // console.log(words[i]);
+      words[i] = noback(words[i]);
+      words[i] = noNew(words[i]);
+      var wordarr = words[i].split(' ');
+      if (wordarr.length > 1){
+        for (var j = 1; j<wordarr.length; j++){
+          words.push(wordarr[j]);
+          words[i] = wordarr[0];
+        } 
+      }
+
       //using the word as a hash-key, its value is the number of 
       //occurrences of the key-word in each document
       if (words[i] in returnObj.wordtable){
@@ -55,4 +79,104 @@ module.exports.makeJSON = function(str){
     }
   }
 return returnObj;
+}
+var commonWords = {
+  the:null,
+  be:null,
+  to:null,
+  of:null,
+  and:null,
+  a:null,
+  in:null,
+  that:null,
+  have:null,
+  I:null,
+  it:null,
+  for:null,
+  not:null,
+  on:null,
+  with:null,
+  he:null,
+  as:null,
+  you:null,
+  do:null,
+  at:null,
+  Word:null,
+  this:null,
+  but:null,
+  his:null,
+  by:null,
+  from:null,
+  they:null,
+  we:null,
+  say:null,
+  her:null,
+  she:null,
+  or:null,
+  an:null,
+  will:null,
+  my:null,
+  one:null,
+  all:null,
+  would:null,
+  there:null,
+  their:null,
+  what:null,
+  Word:null,
+  so:null,
+  up:null,
+  out:null,
+  if:null,
+  about:null,
+  who:null,
+  get:null,
+  which:null,
+  go:null,
+  me:null,
+  when:null,
+  make:null,
+  can:null,
+  like:null,
+  time:null,
+  no:null,
+  just:null,
+  him:null,
+  know:null,
+  take:null,
+  Word:null,
+  people:null,
+  into:null,
+  year:null,
+  your:null,
+  good:null,
+  some:null,
+  could:null,
+  them:null,
+  see:null,
+  other:null,
+  than:null,
+  then:null,
+  now:null,
+  look:null,
+  only:null,
+  come:null,
+  its:null,
+  over:null,
+  think:null,
+  also:null,
+  Word:null,
+  back:null,
+  after:null,
+  use:null,
+  two:null,
+  how:null,
+  our:null,
+  work:null,
+  first:null,
+  well:null,
+  way:null,
+  even:null,
+  new:null,
+  want:null,
+  because:null 
 }
